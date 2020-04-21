c6c2ebb9587f884ff5e189614e69c13a22a2d0b2
Marcus Kern <skyacem@hotmail.com>
2020-04-21 15:10:48 -0500

    Started Profile Dev designed around 1drv storage

---
+++ PersonalProfileGenerateModuleManifest.ps1
@@ -0,0 +1,132 @@
+# New-ModuleManifest  -Path "C:\Users\marcus\OneDrive\Scripts\Powershell\Profiles\Scripts\PersonalProfileModule\PSD1 - Module Manifest\PersonalProfile-Module-1.psd1" -PassThru
+$PersonalProfileManifestDefinitionPath = "C:\Users\marcus\OneDrive\Scripts\Powershell\Profiles\Scripts\PersonalProfileModule\PSD1 - Module Manifest\PersonalProfile-Module-1.psd1"
+$PersonalProfileManifestDefinition = 
+@{
+
+    # Script module or binary module file associated with this manifest.
+    # RootModule = ''
+
+    # Version number of this module.
+    ModuleVersion          = '0.0.0.1'
+
+    # Supported PSEditions
+    CompatiblePSEditions   = @("6", "7")
+
+    # ID used to uniquely identify this module
+    GUID                   = '072a795d-3f71-46de-af56-4b1bf5b0639f'
+
+    # Author of this module
+    Author                 = 'Marcus Kern'
+
+    # Company or vendor of this module
+    CompanyName            = 'Skyhoshi Games'
+
+    # Copyright statement for this module
+    Copyright              = '(c) Marcus Kern. All rights reserved.'
+
+    # Description of the functionality provided by this module
+    Description            = 'Powershell Module to manage Profile scripts. Load, Reload, and Expand Scripts within your script folder'
+
+    # Minimum version of the PowerShell engine required by this module
+    PowerShellVersion      = '5'
+
+    # Name of the PowerShell host required by this module
+    # PowerShellHostName = ''
+
+    # Minimum version of the PowerShell host required by this module
+    # PowerShellHostVersion = ''
+
+    # Minimum version of Microsoft .NET Framework required by this module. This prerequisite is valid for the PowerShell Desktop edition only.
+    DotNetFrameworkVersion = '4.8'
+
+    # Minimum version of the common language runtime (CLR) required by this module. This prerequisite is valid for the PowerShell Desktop edition only.
+    # CLRVersion = ''
+
+    # Processor architecture (None, X86, Amd64) required by this module
+    ProcessorArchitecture  = 'None'
+
+    # Modules that must be imported into the global environment prior to importing this module
+    RequiredModules        = @("Microsoft.PowerShell.Utility", "Microsoft.PowerShell.Management", "OneDrive")
+
+    # Assemblies that must be loaded prior to importing this module
+    # RequiredAssemblies = @()
+
+    # Script files (.ps1) that are run in the caller's environment prior to importing this module.
+    ScriptsToProcess       = @("PersonalProfileMethods.ps1")
+
+    # Type files (.ps1xml) to be loaded when importing this module
+    # TypesToProcess = @()
+
+    # Format files (.ps1xml) to be loaded when importing this module
+    # FormatsToProcess = @()
+
+    # Modules to import as nested modules of the module specified in RootModule/ModuleToProcess
+    # NestedModules = @()
+
+    # Functions to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no functions to export.
+    FunctionsToExport      = @(Redo-PersonalProfile Reset-PersonalProfileScripts New-PersonalProfile)
+
+    # Cmdlets to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no cmdlets to export.
+    CmdletsToExport        = @()
+
+    # Variables to export from this module
+    VariablesToExport      = '*'
+
+    # Aliases to export from this module, for best performance, do not use wildcards and do not delete the entry, use an empty array if there are no aliases to export.
+    AliasesToExport        = @()
+
+    # DSC resources to export from this module
+    # DscResourcesToExport = @()
+
+    # List of all modules packaged with this module
+    ModuleList             = @("PersonalProfile-Module")
+
+    # List of all files packaged with this module
+    FileList               = @("PersonalProfileMethods.psm1")
+
+    # Private data to pass to the module specified in RootModule/ModuleToProcess. This may also contain a PSData hashtable with additional module metadata used by PowerShell.
+    PrivateData            = @{
+
+        PSData = @{
+
+            # Tags applied to this module. These help with module discovery in online galleries.
+            Tags                       = @("ProfileManagement", "Profile", "Management", "OneDrive" )
+
+            # A URL to the license for this module.
+            LicenseUri                 = 'https://github.com/skyhoshi/PersonalProfileModule/blob/master/LICENSE'
+
+            # A URL to the main website for this project.
+            ProjectUri                 = 'https://github.com/skyhoshi/PersonalProfileModule'
+
+            # A URL to an icon representing this module.
+            IconUri                    = 'https://github.com/skyhoshi/PersonalProfileModule/blob/master/docs/imgs/icons/module-icon.png'
+
+            # ReleaseNotes of this module
+            ReleaseNotes               = 'https://github.com/skyhoshi/PersonalProfileModule/releases'
+
+            # Prerelease string of this module
+            # Prerelease = ''
+
+            # Flag to indicate whether the module requires explicit user acceptance for install/update/save
+            RequireLicenseAcceptance   = $true
+
+            # External dependent modules of this module
+            ExternalModuleDependencies = @()
+
+        } # End of PSData hashtable
+
+    } # End of PrivateData hashtable
+
+    # HelpInfo URI of this module
+    HelpInfoURI = 'https://github.com/skyhoshi/PersonalProfileModule/issues'
+
+    # Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
+    # DefaultCommandPrefix = ''
+
+}
+New-ModuleManifest -CompanyName
+-Guid $PersonalProfileManifestDefinition.GUID
+-NestedModules $PersonalProfileManifestDefinition.NestedModules
+-Path $PersonalProfileManifestDefinitionPath 
+-Author $PersonalProfileManifestDefinition.Author
+
---
+++ PersonalProfileMethods.ps1
@@ -0,0 +1,73 @@
+
+$ProfileInfoStorage = "$env:OneDriveConsumer\Scripts\Powershell\Profiles\XML-JSON Storage\Profile-Info\$env:COMPUTERNAME\"
+function Redo-PersonalProfile {
+    @(
+        $Profile.AllUsersAllHosts,
+        $Profile.AllUsersCurrentHost,
+        $Profile.CurrentUserAllHosts,
+        $Profile.CurrentUserCurrentHost
+    ) | ForEach-Object {
+        if (Test-Path $_) {
+            Write-Verbose "Running $_"
+            . $_
+        }
+    }
+    Reset-ProfileScripts
+<#
+.SYNOPSIS
+
+Reloads all Powershell Profile files
+
+.DESCRIPTION
+
+Reloads all Powershell Profile files by first removing custom modules and running the profile again without restarting the host.
+
+.INPUTS
+
+None. You cannot pipe objects to Redo-Profile.
+
+.OUTPUTS
+
+None. This will reload all Profile scripts.
+
+
+.EXAMPLE
+
+PS> Redo-Profile
+
+.LINK
+
+https://github.com/skyhoshi/PersonalProfileModule
+
+#. LINK
+
+Set-Item
+#>
+}
+
+function Reset-PersonalProfileScripts {
+    . "$env:OneDriveConsumer\Scripts\Powershell\Profiles\Scripts\SystemProfiles\SymbolicLinks\SetupProfileSymbolicLinks.ps1"
+    . "$env:OneDriveConsumer\Scripts\Powershell\Profiles\Scripts\Open-SSH\RobotSSH.ps1"
+}
+function New-PersonalProfile {
+    <#
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $profileName
+    )
+    if (!(Test-Path -Path $profileName)) {
+        New-Item -ItemType File -Path $profileName -Force
+    }
+#>
+}
+
+function Register-PersonalProfileManager{
+    
+
+}
+
+function Register-LocalPersonalProfileManager{
+    Register-PSRepository -Name SorafamilyNuGetRepo -SourceLocation http://nuget.sorafamily.com/nuget/ -ScriptSourceLocation http://nuget.sorafamily.com/nuget -InstallationPolicy Trusted
+}
